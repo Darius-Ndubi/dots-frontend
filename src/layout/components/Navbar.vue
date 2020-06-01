065e4c5de6fa141b30ee5fa50d325a5aba2cff87
Andrew <andrew.tc.pham@gmail.com>
2020-06-01 02:42:45 +0200

    style register page (#112)

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -22,7 +22,7 @@
       >{{ item.label }}</router-link>
     </div>
     <div class="avatar-wrapper" @click="toggleDrawer">
-      <el-avatar shape="square">{{ nameInitials }}</el-avatar>
+      <el-avatar>{{ nameInitials }}</el-avatar>
     </div>
   </top-nav>
 </template>
